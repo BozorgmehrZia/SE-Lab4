ce3c273e76e9b0702eedcc6f50cb3c0341e5aa92
li-sadeghi <a.sadeghiali@yahoo.com>
2024-08-07 03:42:32 +0330

    feature(BDD): Add BDD for DSR (Dividing and Square Root for two numbers) and complete the tests.

--- BDD/src/main/java/calculator/Calculator.java
+++ BDD/src/main/java/calculator/Calculator.java
@@ -1,6 +1,9 @@
 package calculator;
 
 public class Calculator {
+    public double dividingAndSquareRoot(int a, int b) {
+        return Math.sqrt((double) a/b);
+    }
     public int add(int a, int b) {
         return a + b;
     }
